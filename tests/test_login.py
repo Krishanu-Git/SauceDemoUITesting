7e69cc3a7e93dc4a61add3634ecdf7a88f2525cc
Chinmay Mule <cmule@netskope.com>
2026-02-24 11:16:51 +0530

    Added login page for sauce demo website UI testing

---
+++ tests/test_login.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page
+from Pages import LoginPage
+import pytest
+
+def test_valid_login(page: Page):
+    login_page = LoginPage(page)
+    login_page.goto_saucedemo_web()
+    print("the webpage is opened")
+    page.wait_for_timeout(1000)
+    login_page.fill_login_form('standard_user', 'secret_sauce')
+    print("Filled the login form")
+    login_page.click_login_btn()
+    print("clicked login btn")
