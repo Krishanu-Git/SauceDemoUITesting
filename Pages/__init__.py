6dd1afac34ae7ac20f1e4ef23ba607e6e156a292
Chinmay Mule <cmule@netskope.com>
2026-03-02 15:45:46 +0530

    added Inventory Item Page POM and test cases related to that

--- Pages/__init__.py
+++ Pages/__init__.py
@@ -1,2 +1,3 @@
 from .Login import LoginPage
 from .AddtoCart import AddtoCartPage
+from .InventoryItem import InventoryItemPage
--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -1,16 +1,19 @@
 from playwright.sync_api import Page
-from Pages import LoginPage, AddtoCartPage
+from Pages import LoginPage, AddtoCartPage, InventoryItemPage
 
 def test_add_to_cart(page: Page):
     login_page = LoginPage(page)
-    add_to_cart_page = AddtoCartPage(page)
+    inventory_page = InventoryItemPage(page)
 
     login_page.goto_saucedemo_web()
-    page.wait_for_timeout(1000)
     login_page.fill_login_form('standard_user', 'secret_sauce')
     login_page.click_login_btn()
     page.wait_for_timeout(1000)
-
-    add_to_cart_page.click_add_to_cart_btn()
-    item_price = add_to_cart_page.get_item_price()
-    print(f"The price of the item is: {item_price}")
+    all_items_name = inventory_page.get_all_items()
+    print(all_items_name)
+    for i in range(len(all_items_name)):
+        print(inventory_page.get_img_link(all_items_name[i]))
+        print(inventory_page.get_item_description(all_items_name[i]))
+        print(inventory_page.get_item_price(all_items_name[i]))
+        inventory_page.click_add_to_cart_btn(all_items_name[i])
+        # inventory_page.click_remove_from_cart_btn(all_items_name[i])
